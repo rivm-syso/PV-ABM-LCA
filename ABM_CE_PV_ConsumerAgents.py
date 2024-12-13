fae6d58721ee65478b11d30f188c337c30b4b5cb
agnesefuortes <agnese.fuortes@gmail.com>
2024-12-13 21:02:47 +0100

    Added effect to attitude towards used panels

--- ABM_CE_PV_ConsumerAgents.py
+++ ABM_CE_PV_ConsumerAgents.py
@@ -385,20 +385,20 @@ class Consumers(Agent):
         Calculate pro-environmental attitude component of EoL TPB rule. Options
         considered pro environmental get a higher score than other options.
         """
+        impact_mult = 1 + self.model.impact_effect()
         for i in range(len(att_levels)):
             if decision == "EoL_pathway":
                 if list(self.model.all_EoL_pathways.keys())[i] == "repair" or \
                         list(self.model.all_EoL_pathways.keys())[i] == "sell" \
                         or list(self.model.all_EoL_pathways.keys())[i] == \
                         "recycle":
-                    impact_mult = 1 + self.model.impact_effect()
                     att_levels[i] = min(att_level * impact_mult, 1) ##max out att_level at 1
                 else:
                     att_levels[i] = 1 - min(att_level * impact_mult, 1)
             elif decision == "purchase_choice":
                 if self.purchase_choices[i] == "used" or \
                         self.purchase_choices[i] == "certified":
-                    att_levels[i] = att_level
+                    att_levels[i] = min(att_level * impact_mult, 1) ##max out att_level at 1
                 else:
                     att_levels[i] = 1 - att_level
         return [weight_a * x for x in att_levels]
